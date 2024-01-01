933e21b60d80053e9a5e76d81a0e036b7fecba5e
benny-jacc <bernardo.jacob.amorim@outlook.com>
2024-01-01 14:25:54 +0000

    Adding Conquer of Completion and COC-RUST

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,21 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "c", "lua", "rust", "ruby", "vim" },
+      ensure_installed = { "c", "lua", "rust", "ruby", "vim", "toml" },
       sync_install = false,
       auto_install = true,
-      highlight = { enable = true, },
+      highlight = { 
+        enable = true, 
+        additional_vim_regex_highlighting = false,
+      },
+      ident = { 
+        enable = true,
+      },
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+      },
     }
   end
 }
